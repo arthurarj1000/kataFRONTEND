129677c1b4c258c399c820da8d73eeb5ee7a97f1
arthurarj1000 <arturoarjim1000@gmail.com>
2024-10-22 21:17:20 -0600

    Clase 1

---
+++ 1-node-npm/1.4-api-express/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//list songs
+const songs = [
+    {id: 1, name: "El Azul", artist: 'Peso Pluma', year: 2022},
+    {id: 2, name: "SEDA", artist: 'Bad Bunny', year: 2023},
+    {id: 3, name: "LOKERON", artist: 'Tito Doble P', year: 2024},
+    {id: 4, name: "Marvins Room", artist: 'Drake', year: 2017},
+];
+
+
+
+// /songs: lista canciones
+//url - endpoint getSongs
+app.get('/', (req, res) =>{
+    res.json(songs);
+});
+
+app.listen(port, ()=>{
+    console.log('Servidor de express está corriendo', port);
+});
